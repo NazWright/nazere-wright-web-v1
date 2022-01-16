76927e90eb742f76bd21cfd4de7325e45a979e80
Nazere Wright <nazwrightthedeveloper@gmail.com>
2022-01-16 14:20:36 -0500

    back to square one :)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
+import "./App.css";
+import Home from "./components/home/Home";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
