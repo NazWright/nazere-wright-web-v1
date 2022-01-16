20b57b5edcb0c40b9e4f639991675f026a3bce7a
Nazere Wright <nazwrightthedeveloper@gmail.com>
2022-01-16 17:03:48 -0500

    changes

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const app = express();
 
 app.use(express.json());
 
-// if (process.env.NODE_ENV === "production") {
-//   // making sure express will serve up production assets
-//   app.use(express.static("frontend/build"));
-//   // express will serve up index.html
-//   const path = require("path");
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  // making sure express will serve up production assets
+  app.use(express.static("frontend/build"));
+  // express will serve up index.html
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Listening"));
