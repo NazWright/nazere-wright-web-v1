2279c4fee92b2ed80db1d904b16066434d732160
Nazere Wright <nazwrightthedeveloper@gmail.com>
2022-01-16 16:52:58 -0500

    changes

--- frontend/src/components/home/Home.test.js
+++
@@ -1,5 +0,0 @@
-import { navLinks } from "../../constants/navigationConstants";
-
-describe("testing the home component rendering", () => {
-  it("checks to see if the top navbar is rendered on the screen", () => {});
-});
--- frontend/src/constants/navigationConstants.js
+++
@@ -1,8 +0,0 @@
-const navLinks = [
-  { name: "Projects", route: "/projects" },
-  { name: "Skills", route: "/skills" },
-  { name: "Services", route: "/services" },
-  { name: "Contact", route: "/contact" },
-];
-
-export const navLinks; 
