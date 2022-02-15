cb364cb938fa908fee654660fc64502e4095a26b
Nazere Wright <nazwrightthedeveloper@gmail.com>
2022-02-14 20:30:54 -0500

    adding poster to mobile

--- frontend/src/components/videobackground/VideoBackground.js
+++ frontend/src/components/videobackground/VideoBackground.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./VideoBackground.css";
+import poster from "./mobilecodingposter.svg";
 
 export default function VideoBackground() {
   return (
-    <video autoPlay loop muted id="background-video">
+    <video autoPlay loop muted id="background-video" poster={poster}>
       <source type="video/mp4" src={require("./codingvidres.mp4")}></source>
     </video>
   );
