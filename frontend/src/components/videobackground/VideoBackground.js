b7aeb735d3eca9287197e1b5b3fa94fd07b77adb
Nazere Wright <nazwrightthedeveloper@gmail.com>
2022-02-14 21:04:07 -0500

    fdfd

--- frontend/src/components/videobackground/VideoBackground.js
+++ frontend/src/components/videobackground/VideoBackground.js
@@ -2,7 +2,22 @@ import React from "react";
 import "./VideoBackground.css";
 import poster from "./mobilecodingposter.svg";
 
-export default function VideoBackground() {
+export default function VideoBackground({ src }) {
+  if (!src) {
+    return (
+      <div
+        style={{
+          backgroundImage: `url(${poster})`,
+          width: "100%",
+          position: "absolute",
+          zIndex: -1,
+          height: "275vh",
+          backgroundSize: "cover",
+        }}
+      ></div>
+    );
+  }
+
   return (
     <video autoPlay loop muted id="background-video" poster={poster}>
       <source type="video/mp4" src={require("./codingvidres.mp4")}></source>
