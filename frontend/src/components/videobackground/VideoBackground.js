202f1e75ecd2724d8581c1539a1fd598d2939f0b
Nazere Wright <nazwrightthedeveloper@gmail.com>
2022-02-14 21:11:10 -0500

    changes

--- frontend/src/components/videobackground/VideoBackground.js
+++ frontend/src/components/videobackground/VideoBackground.js
@@ -3,21 +3,6 @@ import "./VideoBackground.css";
 import poster from "./mobilecodingposter.svg";
 
 export default function VideoBackground({ src }) {
-  if (!src) {
-    return (
-      <div
-        style={{
-          backgroundImage: `url(${poster})`,
-          width: "100%",
-          position: "absolute",
-          zIndex: -1,
-          height: "275vh",
-          backgroundSize: "cover",
-        }}
-      ></div>
-    );
-  }
-
   return (
     <video autoPlay loop muted id="background-video" poster={poster}>
       <source type="video/mp4" src={require("./codingvidres.mp4")}></source>
