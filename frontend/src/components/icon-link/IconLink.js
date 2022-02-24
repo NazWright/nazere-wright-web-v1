e75cc54cdd1cb5e7109215dcfd519d5c1e1da89d
Nazere Wright <nazwrightthedeveloper@gmail.com>
2022-02-24 11:08:04 -0500

    projects

---
+++ frontend/src/components/icon-link/IconLink.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function IconLink({ labelText, icon, style, URL }) {
+  return <div>IconLink</div>;
+}
--- frontend/src/components/projects/SingleProject.js
+++ frontend/src/components/projects/SingleProject.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import { FaLink, FaGithub } from "react-icons/fa";
 
 export default function SingleProject({
   projectName,
@@ -22,13 +23,19 @@ export default function SingleProject({
     },
   };
 
+  const navigateToLink = (link) => {
+    window.location.href = link;
+  };
+
   const defaultContent = () => {
     return (
       <div>
         <img
           width={"100%"}
+          height={deviceType === "desktop" ? 215 : undefined}
           style={{
             borderRadius: "10px",
+            border: "1px solid black",
           }}
           src={projectImage}
         ></img>
@@ -36,38 +43,73 @@ export default function SingleProject({
           style={{
             fontSize:
               deviceType === "mobile" || deviceType === "tablet"
-                ? "4vw"
-                : "1.5vw",
+                ? "6vw"
+                : "2vw",
             fontWeight: "bold",
-            marginTop: "1rem",
+            margin: "2rem",
+            height: 50,
           }}
         >
           {" "}
           {projectName}{" "}
         </h2>
         <div>
-          <a
-            className="project-link"
-            style={{
-              display: projectLink ? undefined : "none",
-              textDecoration: "underline",
-            }}
-            href={projectLink}
-          >
-            Website
-          </a>
+          {projectLink ? (
+            <div
+              className="github-link-container"
+              onClick={() => navigateToLink(projectLink)}
+            >
+              {" "}
+              <FaLink
+                size={
+                  deviceType === "mobile" || deviceType === "tablet"
+                    ? "6vw"
+                    : "2.2vw"
+                }
+                className="github-link-icon"
+              />{" "}
+              <span
+                className="link-icon-text"
+                style={{
+                  fontSize:
+                    deviceType === "mobile" || deviceType === "tablet"
+                      ? "4vw"
+                      : "1.5vw",
+                }}
+              >
+                Website
+              </span>{" "}
+            </div>
+          ) : undefined}
         </div>
         <div>
-          <a
-            className="project-link"
-            style={{
-              display: projectGithub ? undefined : "none",
-              textDecoration: "underline",
-            }}
-            href={projectGithub}
-          >
-            Github
-          </a>
+          {projectGithub ? (
+            <div
+              className="github-link-container"
+              onClick={() => navigateToLink(projectGithub)}
+            >
+              {" "}
+              <FaGithub
+                size={
+                  deviceType === "mobile" || deviceType === "tablet"
+                    ? "6vw"
+                    : "2.2vw"
+                }
+                className="github-link-icon"
+              />{" "}
+              <span
+                className="link-icon-text"
+                style={{
+                  fontSize:
+                    deviceType === "mobile" || deviceType === "tablet"
+                      ? "4vw"
+                      : "1.5vw",
+                }}
+              >
+                GitHub
+              </span>{" "}
+            </div>
+          ) : undefined}
         </div>
       </div>
     );
