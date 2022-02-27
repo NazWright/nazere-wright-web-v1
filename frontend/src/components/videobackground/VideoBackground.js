c438b374d39ea81ebcde24affa59422b8e2aafe6
Nazere Wright <nazwrightthedeveloper@gmail.com>
2022-02-27 12:06:39 -0500

    hopefully mobile phone video plays

--- frontend/src/components/videobackground/VideoBackground.js
+++ frontend/src/components/videobackground/VideoBackground.js
@@ -4,7 +4,14 @@ import poster from "./mobilecodingposter.svg";
 
 export default function VideoBackground({ src }) {
   return (
-    <video autoPlay loop muted id="background-video" poster={poster}>
+    <video
+      playsInline
+      autoPlay
+      loop
+      muted
+      id="background-video"
+      poster={poster}
+    >
       <source type="video/mp4" src={require("./codingvidres.mp4")}></source>
     </video>
   );
