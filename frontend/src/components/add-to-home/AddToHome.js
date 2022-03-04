88d43b337dcf7f6b3f393d2c8464878bdbc6d49d
Nazere Wright <nazwrightthedeveloper@gmail.com>
2022-03-04 13:22:23 -0500

    only on desktops

--- frontend/src/components/add-to-home/AddToHome.js
+++ frontend/src/components/add-to-home/AddToHome.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function AddToHome() {
+  // only show this component on the desktop. This button is not compatible on mobile
+  const { deviceType } = useSelector((state) => state.control);
   return (
     <div>
-      <button id="add-to-home">Add App To Home Screen</button>
+      {deviceType === "desktop" && (
+        <button id="add-to-home">Add App To Home Screen</button>
+      )}
     </div>
   );
 }
