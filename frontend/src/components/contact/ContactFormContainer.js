cae05e0d1b7265e7a2568271098a0f7112d8f5e0
Nazere Wright <nazwrightthedeveloper@gmail.com>
2022-03-20 12:07:04 -0400

    cleaner form logic

--- frontend/src/components/contact/ContactFormContainer.js
+++ frontend/src/components/contact/ContactFormContainer.js
@@ -10,15 +10,32 @@ class ContactFormContainer extends Component {
       <ContactForm
         handleSubmit={this.handleSubmit}
         onInputChange={this.onChange}
+        submitted={this.state.submitted}
       />
     );
   }
   // handle the submission of the contact form
   handleSubmit(event) {
     event.preventDefault();
+    event.target.disabled = true;
     console.log(this.state);
     this.props.dispatch(sendContactMessage(this.state));
+    alert(
+      "Thank you for submitting your contacting me. I will email in response shortly"
+    );
+    // resetting all of the form values
+    event.target.reset();
+    event.target.disabled = false;
   }
+
+  intialState = {
+    name: "",
+    reason: "",
+    phoneNumber: "",
+    email: "",
+    notes: "",
+    submitted: false,
+  };
 
   //   change a value in the state.
   onChange(property, value) {
@@ -27,13 +44,7 @@ class ContactFormContainer extends Component {
 
   constructor(props) {
     super();
-    this.state = {
-      name: "",
-      reason: "",
-      phoneNumber: "",
-      email: "",
-      notes: "",
-    };
+    this.state = this.intialState;
     // binding the functions
     this.onChange = this.onChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
