4d8cc44425ef995bac4b8a4ae39b862c14cd7633
Nazere Wright <nazwrightthedeveloper@gmail.com>
2022-03-22 17:51:55 -0400

    changes

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,17 +1,9 @@
 import React from "react";
 import "./Header.css";
-import { useSelector } from "react-redux";
-import AddToHome from "../add-to-home/AddToHome";
 
 export default function Header() {
-  const { deviceType } = useSelector((state) => state.control);
-
-  const deviceIsDesktop = deviceType === "desktop";
-
   const name = "Nazere Wright";
   const tagline = "Software Engineer";
-  const desc =
-    "I am a Software Engineer with a specialty in building web applications and creating memorable digital experiences.";
 
   return (
     <section className="header-section">
